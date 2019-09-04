eb30ca9fb5f5cd27a3a7adddce9d2c3aae15fdcc
Даулет Алмагамбетов <almagambetov@surfstudio.co>
2019-09-04 17:24:41 +0500

    feat: add tableView

--- InstagramHome/Models/Post.swift
+++ InstagramHome/Models/Post.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 daukabase. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct Post {
     let authorNickname: String
--- InstagramHome/Modules/Home/HomeViewController.swift
+++ InstagramHome/Modules/Home/HomeViewController.swift
@@ -10,8 +10,31 @@ import UIKit
 
 final class HomeViewController: UIViewController {
     
+    let posts: [Post] = []
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
     
 }
