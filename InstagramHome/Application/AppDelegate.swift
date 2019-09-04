9e72285e4a0635014924fb01043dbbe339fc6b50
Даулет Алмагамбетов <almagambetov@surfstudio.co>
2019-09-04 17:18:43 +0500

    feat: intialize project

---
+++ InstagramHome/Application/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  InstagramHome
+//
+//  Created by Daulet on 04/09/2019.
+//  Copyright © 2019 daukabase. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        return true
+    }
+
+
+}
+
