965d7f2d4ed51b98c423f1ef8650093e75fe9e74
Даулет Алмагамбетов <almagambetov@surfstudio.co>
2019-09-04 17:21:38 +0500

    feat: configure podfile

---
+++ InstagramHome/Modules/Home/HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  InstagramHome
+//
+//  Created by Daulet on 04/09/2019.
+//  Copyright © 2019 daukabase. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+}
